e5c1d8d736102c71c88499a315c54047b99ec667
Sebaa71 <129334281+Sebaa71@users.noreply.github.com>
2023-12-08 18:05:26 -0500

    Hour Changes

--- script.js
+++ script.js
@@ -316,7 +316,7 @@ function toggleLoading() {
 function getTimeOfDay(hour) {
   if (hour >= 6 && hour < 12) {
     return "morning";
-  } else if (hour >= 12 && hour < 18) {
+  } else if (hour >= 12 && hour < 19) {
     return "day";
   } else {
     return "night";
